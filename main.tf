5e9fdb6b080c2b61e83af4cb82813a2689d2ec06
ayushigupta <ayushi.gupta@joingsg.com>
2023-01-03 18:31:53 +0100

    updated aws_ami image

--- main.tf
+++ main.tf
@@ -1,22 +1,27 @@
-data "aws_ami" "myami" {
-    most_recent = true
-    executable_users = ["274055544780"]
+data "aws_ami" "example" {
+    executable_users = ["self"]
+    most_recent      = true
+    name_regex       = "^myami-\\d{3}"
+    owners           = ["self"]
 
     filter {
-        name   = "name"
-        values = ["myami-*"]
+    name   = "name"
+    values = ["myami-*"]
     }
 
     filter {
-        name = "virtualization-type"
-        values = ["hvm"]
+    name   = "root-device-type"
+    values = ["ebs"]
     }
 
-    owners = ["274055544780"] # GSG-playground
+    filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+    }
 }
 
 resource "aws_instance" "web" {
-    ami           = data.aws_ami.myami.id
+    ami           = data.aws_ami.example.id
     instance_type = "t3.nano"
 
     tags = {
