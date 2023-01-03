a59b5e6153816d23a719ccc1bff5af38ce680363
ayushigupta <ayushi.gupta@joingsg.com>
2023-01-03 18:21:32 +0100

    updated aws_ami image specs

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-data "aws_ami" "myami" {
+data "aws_ami" "amazon-linux-2" {
   most_recent = true
   executable_users = ["274055544780"]
 
   filter {
     name   = "name"
-    values = ["myami-*"]
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
 
   filter {
@@ -17,11 +17,11 @@ data "aws_ami" "myami" {
     values = ["hvm"]
   }
 
-  owners = ["274055544780"] # GSG-playground
+  owners = ["amazon"] # GSG-playground
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.myami.id
+  ami           = data.aws_ami.amazon-linux-2.id
   instance_type = "t3.nano"
 
   tags = {
