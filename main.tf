a3ec72ae8804aedd395883b62eb621a281a7c81e
ayushigupta <ayushi.gupta@joingsg.com>
2023-01-03 18:23:44 +0100

    updated aws_ami image specs

--- main.tf
+++ main.tf
@@ -1,30 +1,25 @@
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-  executable_users = ["274055544780"]
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    executable_users = ["274055544780"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
-  }
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"] # GSG-playground
+    owners = ["amazon"] # GSG-playground
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.amazon-linux-2.id
-  instance_type = "t3.nano"
+    ami           = data.aws_ami.ubuntu.id
+    instance_type = "t3.nano"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+    tags = {
+        Name = "HelloWorld"
+    }
 }
