f57d987cd4c0c1d54e792410fdbc04f2b03e514f
ayushigupta <ayushi.gupta@joingsg.com>
2023-01-04 17:11:04 +0100

    added variables

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "amazon-linux-2" {
 
 resource "aws_instance" "web" {
     ami           = data.aws_ami.amazon-linux-2.id
-    instance_type = "t3.micro"
+    instance_type = var.instance_type
 
     tags = {
         Name = "HelloWorld"
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "instance_type" {
+    description = "Type of instance to provision"
+    default = "t3.nano"
+}
