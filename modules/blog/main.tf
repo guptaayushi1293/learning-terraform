9ddfa1bc1d90b11591a361ea9609ad866f31abab
ayushigupta <ayushi.gupta@joingsg.com>
2023-01-06 14:40:11 +0100

    added files to directory

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -13,17 +13,6 @@ data "aws_ami" "amazon-linux-2" {
     }
 }
 
-# resource "aws_instance" "blog" {
-#   ami                    = data.aws_ami.amazon-linux-2.id
-#   instance_type          = var.instance_type
-#   subnet_id              = module.blog_vpc.public_subnets[0]
-#   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-#   tags = {
-#     Name = "Learning Terraform"
-#   }
-# }
-
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
